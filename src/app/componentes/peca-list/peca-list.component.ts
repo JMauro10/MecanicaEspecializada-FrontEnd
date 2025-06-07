5b0eb4bea0fe7748765bdf26d68e7b620d90d4e6
SC-Alexandre <alexandre.sc115@gmail.com>
2025-06-07 14:43:36 -0300

    Peca e Servico

---
+++ src/app/componentes/peca-list/peca-list.component.ts
@@ -0,0 +1,108 @@
+import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import {Peca} from '../../models/peca';
+import {PecaService} from '../../service/peca.service';
+import {Button} from 'primeng/button';
+import {Dialog} from 'primeng/dialog';
+import {FormsModule} from '@angular/forms';
+import {Panel} from 'primeng/panel';
+
+@Component({
+  selector: 'app-peca-list',
+  imports: [TableModule, PaginatorModule, Button, Dialog, FormsModule, Panel],
+  templateUrl: './peca-list.component.html',
+  styleUrl: './peca-list.component.css'
+})
+
+export class PecaListComponent {
+  mostrarDialogPeca = false;
+  novaPeca: Peca = {codigo:"",descricao:"",valorUnitario:0,quantidade:0 };
+  listaPecas: Peca[] = [];
+
+  constructor(private pecaService:PecaService) {
+    this.pecaService.listarPeca().subscribe(peca => this.listaPecas = peca);
+  }
+
+  onPecaChange(){
+    this.mostrarDialogPeca = true;
+  }
+
+  adicionarPeca(){
+    if(!this.novaPeca.codigo.trim()){
+      alert('O código é obrigatório!');
+      return;
+    }
+    if(!this.novaPeca.descricao.trim()){
+      alert('A descrição é obrigatória!')
+      return;
+    }
+    if (
+      this.novaPeca.quantidade === null ||
+      this.novaPeca.quantidade === undefined ||
+      isNaN(this.novaPeca.quantidade) ||
+      this.novaPeca.quantidade <= 0
+    ) {
+      alert('A quantidade deve ser um número maior que zero!');
+      return;
+    }
+    if (
+      this.novaPeca.valorUnitario === null ||
+      this.novaPeca.valorUnitario === undefined ||
+      isNaN(this.novaPeca.valorUnitario) ||
+      this.novaPeca.valorUnitario <= 0
+    ) {
+      alert('O valor unitário deve ser maior que zero!');
+      return;
+    }
+    console.log('Dados do formulário antes do envio:', this.novaPeca);
+
+    this.pecaService.incluirPeca(this.novaPeca).subscribe({
+      next: (peca) => {
+        console.log('Peca cadastrada com sucesso!');
+        alert('Peça cadastrada com sucesso!');
+        this.atualizarListaPeca();
+        this.novaPeca = {
+          codigo: '',
+          descricao: '',
+          quantidade: 0,
+          valorUnitario: 0
+        };
+        this.mostrarDialogPeca = false;
+      },
+      error: (erro) => {
+        if (erro.status === 400 || erro.status === 409) {
+          alert(erro.error?.message || 'Já existe uma Peça com esse nome!');
+        } else {
+          alert('Erro inesperado ao cadastrar peça.');
+        }
+      }
+    });
+  }
+
+  atualizarListaPeca(): void {
+    this.pecaService.listarPeca().subscribe(peca => {
+      this.listaPecas = peca;
+    });
+  }
+
+  removerPeca(peca: Peca) {
+    if (peca.id === undefined) {
+      alert("ID da peça não encontrado. Não é possível remover.");
+      return;
+    }
+
+    if (confirm(`Tem certeza que deseja remover a peça "${peca.codigo}"?`)) {
+      this.pecaService.deletarPecaById(peca.id).subscribe({
+        next: () => {
+          // Remove da lista localmente após sucesso:
+          this.listaPecas = this.listaPecas.filter(g => g.id !== peca.id);
+          alert('Peça removida com sucesso!');
+        },
+        error: () => {
+          alert('Ocorreu um erro ao tentar remover a peça.');
+        }
+      });
+    }
+  }
+}
---
+++ src/app/componentes/servico-list/servico-list.component.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import {Button} from 'primeng/button';
+import {Dialog} from 'primeng/dialog';
+import {FormsModule} from '@angular/forms';
+import {Panel} from 'primeng/panel';
+import {Servico} from '../../models/servico';
+import {ServicoService} from '../../service/servico.service';
+
+@Component({
+  selector: 'app-servico-list',
+  imports: [TableModule, PaginatorModule, Button, Dialog, FormsModule, Panel],
+  templateUrl: './servico-list.component.html',
+  styleUrl: './servico-list.component.css'
+})
+export class ServicoListComponent {
+  mostrarDialogServico = false;
+  novoServico: Servico = {descricao:"",valorUnitario:0 };
+  listaServicos: Servico[] = [];
+
+  constructor(private servicoService:ServicoService) {
+    this.servicoService.listarServico().subscribe(servico => this.listaServicos = servico);
+  }
+
+  onServicoChange(){
+    this.mostrarDialogServico = true;
+  }
+
+  adicionarServico(){
+    if(!this.novoServico.descricao.trim()){
+      alert('A descrição é obrigatória!')
+      return;
+    }
+    if (
+      this.novoServico.valorUnitario === null ||
+      this.novoServico.valorUnitario === undefined ||
+      isNaN(this.novoServico.valorUnitario) ||
+      this.novoServico.valorUnitario <= 0
+    ) {
+      alert('O valor unitário deve ser maior que zero!');
+      return;
+    }
+    console.log('Dados do formulário antes do envio:', this.novoServico);
+
+    this.servicoService.incluirServico(this.novoServico).subscribe({
+      next: (servico) => {
+        console.log('Servico cadastrado com sucesso!');
+        alert('Serviço cadastrado com sucesso!');
+        this.atualizarListaServico();
+        this.novoServico = {
+          descricao: '',
+          valorUnitario: 0
+        };
+        this.mostrarDialogServico = false;
+      },
+      error: (erro) => {
+        if (erro.status === 400 || erro.status === 409) {
+          alert(erro.error?.message || 'Já existe um serviço com esse nome!');
+        } else {
+          alert('Erro inesperado ao cadastrar serviço.');
+        }
+      }
+    });
+  }
+
+  atualizarListaServico(): void {
+    this.servicoService.listarServico().subscribe(servico => {
+      this.listaServicos = servico;
+    });
+  }
+
+  removerServico(servico: Servico){
+    if (servico.id === undefined) {
+      alert("ID do serviço não encontrado. Não é possível remover.");
+      return;
+    }
+
+    if (confirm(`Tem certeza que deseja remover o servico "${servico.descricao}"?`)) {
+      this.servicoService.deletarServicoById(servico.id).subscribe({
+        next: () => {
+          // Remove da lista localmente após sucesso:
+          this.listaServicos = this.listaServicos.filter(g => g.id !== servico.id);
+          alert('Serviço removido com sucesso!');
+        },
+        error: () => {
+          alert('Ocorreu um erro ao tentar remover o serviço.');
+        }
+      });
+    }
+  }
+}
