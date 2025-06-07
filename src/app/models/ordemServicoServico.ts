806eef81ebca07209ee0b54a4cb182dc41d99829
SC-Alexandre <alexandre.sc115@gmail.com>
2025-06-07 17:06:11 -0300

    OrdemServicoServico interface e service

---
+++ src/app/models/ordemServicoServico.ts
@@ -0,0 +1,10 @@
+import {Servico} from './servico';
+import {Ordemservico} from './ordemservico';
+
+export interface OrdemServicoServico {
+  id?:number;
+  servico:Servico;
+  ordemServico:Ordemservico;
+  quantidade:number;
+  valorTotal:number;
+}
